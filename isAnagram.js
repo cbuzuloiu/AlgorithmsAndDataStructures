992910e46b822da74c43095fb225e294ef0a3f2b
Cosmin <cbuzuloiu@gmail.com>
2025-01-20 21:35:08 +0200

    commit

---
+++ isAnagram.js
@@ -0,0 +1,12 @@
+"use strict";
+/*
+Given two string, write a function to determine if the second string is an anagram of the first. An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema and iceman
+
+isAnagram('', '') - true
+isAnagram('aaz', 'zza') - false
+isAnagram('anagram', 'nagaram') - true
+isAnagram('rat', 'car') - false
+isAnagram('awesome', 'awesom') - false
+isAnagram('qwerty', 'qeywrt') - true
+isAnagram('texttwisttime', 'timetwisttext') - true
+*/
