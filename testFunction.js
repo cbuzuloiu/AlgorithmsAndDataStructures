eaba894bd51b35fe394a0048b216cdbeef93fcae
Cosmin <cbuzuloiu@gmail.com>
2025-01-20 13:02:19 +0200

    first commit

---
+++ testFunction.js
@@ -0,0 +1,7 @@
+"use strict";
+
+/*
+Write a function caled same, which accept two arrays. The function should return ture if every value in the array has it's corresponding value squared in the second array. The requency values must be the same
+*/
+
+console.log("Hello World");
