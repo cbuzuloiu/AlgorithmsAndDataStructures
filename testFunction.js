e82670484a5566c28014b9ca75696fc6787a22bc
Cosmin <cbuzuloiu@gmail.com>
2025-01-20 13:35:32 +0200

    commit

--- testFunction.js
+++ testFunction.js
@@ -2,6 +2,8 @@
 
 /*
 Write a function caled same, which accept two arrays. The function should return ture if every value in the array has it's corresponding value squared in the second array. The requency values must be the same
-*/
 
-console.log("Hello World");
+same([1,2,3], [4,1,9]) true
+same([1,2,3], [1,9]) false
+same([1,2,1], [4,4,1]) false (must be same frequency)
+*/
