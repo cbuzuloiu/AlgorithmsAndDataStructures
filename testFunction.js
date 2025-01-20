a2ab1f95c9fd2ca759d0dcff529fba5f03e13c28
Cosmin <cbuzuloiu@gmail.com>
2025-01-20 18:31:28 +0200

    commit

--- testFunction.js
+++ testFunction.js
@@ -10,15 +10,29 @@ same([1,2,1], [4,4,1]) false (must be same frequency)
 
 function same(firstArr, secondArr) {
   let coresValueIsSquared = false;
+  let trueValues = 0;
 
   for (let i = 0; i < firstArr.length; i++) {
+    console.log();
     console.log(`=== We are comparing ${firstArr[i]} ===`);
     for (let j = 0; j < secondArr.length; j++) {
       console.log(`Is ${firstArr[i] ** 2} = ${secondArr[j]}?`);
+      if (firstArr[i] ** 2 === secondArr[j]) {
+        console.log("--> YES");
+        trueValues++;
+        console.log(`-- The trueValue count is increased by 1;`);
+        console.log(`-- trueValue is ${trueValues}`);
+      } else {
+        console.log("--> NO");
+      }
     }
   }
-  //sadsa
-  return 0;
+
+  if (trueValues === secondArr.length) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 console.log(same([1, 2, 3], [4, 1, 9]));
