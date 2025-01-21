4c4f4a69e6900e351cc562e0327db9a9766a75a4
Cosmin <cbuzuloiu@gmail.com>
2025-01-21 11:55:20 +0200

    commit

--- isAnagram.js
+++ isAnagram.js
@@ -12,6 +12,7 @@ isAnagram('texttwisttime', 'timetwisttext') - true
 isAnagram("Zaz aap ppa", "zza ppa paa") - true
 */
 
+// === O(N) solution ===
 function isAnagram(str1, str2) {
   if (str1.length !== str2.length) {
     return false;
@@ -44,11 +45,60 @@ function isAnagram(str1, str2) {
   return true;
 }
 
-console.log(isAnagram("", ""));
-console.log(isAnagram("aaz", "zza"));
-console.log(isAnagram("anagram", "nagaram"));
-console.log(isAnagram("rat", "car"));
-console.log(isAnagram("awesome", "awesom"));
-console.log(isAnagram("qwerty", "qeywrt"));
-console.log(isAnagram("texttwisttime", "timetwisttext"));
-console.log(isAnagram("Zaz aap ppa", "zza ppa paa"));
+// console.log(isAnagram("", ""));
+// console.log(isAnagram("aaz", "zza"));
+// console.log(isAnagram("anagram", "nagaram"));
+// console.log(isAnagram("rat", "car"));
+// console.log(isAnagram("awesome", "awesom"));
+// console.log(isAnagram("qwerty", "qeywrt"));
+// console.log(isAnagram("texttwisttime", "timetwisttext"));
+// console.log(isAnagram("Zaz aap ppa", "zza ppa paa"));
+
+// === DIFFERENT SOLUTION ===
+// === O(N) solution ===
+
+function isAnagram2(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+
+  const lookup = {};
+
+  // loop through str1
+  for (let i = 0; i < str1.length; i++) {
+    let letter = str1[i];
+    // if letter exists, incerement, otherwise set to 1
+    lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1);
+  }
+
+  // console.log(`Object with all the elements:`);
+  // console.log(lookup);
+
+  // loop through str2
+  for (let i = 0; i < str2.length; i++) {
+    let letter = str2[i];
+    // console.log(letter);
+    //  can't find letter or letter is zero then it's not an anagram
+    //  zero is a falsy value in JS
+    if (!lookup[letter]) {
+      return false;
+    } else {
+      lookup[letter] -= 1;
+      // console.log(`${letter} - 1 = ${lookup[letter]}`);
+      // console.log(lookup);
+    }
+  }
+  return true;
+}
+
+console.log(isAnagram2("", ""));
+console.log(isAnagram2("aaz", "zza"));
+console.log(isAnagram2("anagram", "nagaram"));
+console.log(isAnagram2("rat", "car"));
+console.log(isAnagram2("awesome", "awesom"));
+console.log(isAnagram2("qwerty", "qeywrt"));
+console.log(isAnagram2("texttwisttime", "timetwisttext"));
+console.log(isAnagram2("Zaz aap ppa", "zza ppa paa"));
