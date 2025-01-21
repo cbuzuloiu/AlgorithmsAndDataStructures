f7e82f6b02efcaf2f5e7e4ec7f9dea2c01723457
Cosmin <cbuzuloiu@gmail.com>
2025-01-21 19:46:02 +0200

    commit

--- maxSubarraySum.js
+++ maxSubarraySum.js
@@ -34,8 +34,60 @@ function maxSubarraySum(arr, n) {
   return maxSum;
 }
 
-console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2));
-console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 4));
-console.log(maxSubarraySum([4, 2, 1, 6], 1));
-console.log(maxSubarraySum([4, 2, 1, 6], 1));
-console.log(maxSubarraySum([], 4));
+// === TIME COMPLEXITY O(N) ===
+function maxSubarraySumFinal(arr, n) {
+  let maxSum = 0;
+  let tempSum = 0;
+
+  if (arr.length < n) {
+    return null;
+  }
+
+  for (let i = 0; i < n; i++) {
+    maxSum += arr[i];
+  }
+
+  tempSum = maxSum;
+  console.log(`First sum is = ${tempSum}`);
+
+  for (let i = n; i < arr.length; i++) {
+    console.log(
+      `Sum previous step: ${tempSum} - element at position ${i - n}: ${
+        arr[i - n]
+      } + element at slided position: ${arr[i]}`
+    );
+    tempSum = tempSum - arr[i - n] + arr[i];
+    console.log(`Current sum is ${tempSum}`);
+    maxSum = Math.max(maxSum, tempSum);
+    console.log(`Max sum is ${maxSum}`);
+    console.log(
+      `=================================================================================`
+    );
+  }
+
+  return maxSum;
+}
+
+// === EXAMPLE ===
+
+// [2,6,9,3,1,8,5,6,3],3 (N=3) maxSum = 19
+
+// STEP 1 - [[2 6 9] 3 1 8 5 6 3]
+// 	        sum of [2 6 9] = 17
+
+//          [a]    [b]
+//           |      |
+// STEP 2 - [2 [6 9 3] 1 8 5 6 3]
+// 	        sum of [6 9 3] = sumStep1 - [a] + [b] = 18
+
+//            [a]    [b]
+//             |      |
+// STEP 3 - [2 6 [9 3 1] 8 5 6 3]
+//          sum of [9 3 1] = sumStep2 - [a] + [b] = 13
+
+console.log(maxSubarraySumFinal([2, 6, 9, 3, 1, 8, 5, 6, 3], 3));
+// console.log(maxSubarraySumFinal([1, 2, 5, 2, 8, 1, 5], 2));
+// console.log(maxSubarraySumFinal([1, 2, 5, 2, 8, 1, 5], 4));
+// console.log(maxSubarraySumFinal([4, 2, 1, 6], 1));
+// console.log(maxSubarraySumFinal([4, 2, 1, 6], 1));
+// console.log(maxSubarraySumFinal([], 4));
